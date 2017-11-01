93a2d36c5dbb93011d4115e4c313c80135280838
Lake <lakesainthomason@gmail.com>
2017-10-31 22:45:41 -0700

    Began Metwear Device Connection
    Implemented some of the connection API

--- app/src/main/java/com/example/lakethomason/bluetoothinteractiontest/MainActivity.java
+++ app/src/main/java/com/example/lakethomason/bluetoothinteractiontest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lakethomason.bluetoothinteractiontest;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -14,14 +15,27 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.os.IBinder;
-import com.mbientlab.metawear.android.BtleService;
+import android.widget.Toast;
 
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.module.Led;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+
+import bolts.Continuation;
+import bolts.Task;
 
 public class MainActivity extends AppCompatActivity implements ServiceConnection {
 
@@ -29,36 +43,19 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     private TextView mHelpText;
     private Button mMetaWearButton;
     private Button mPolarButton;
+    private Button mBlink;
+    private ListView mBluetoothList;
+    private MetaWearBoard board;
 
     BluetoothAdapter mBluetoothAdapter;
-    private final static int REQUEST_ENABLE_BT = 69;
+    private final static int REQUEST_ENABLE_BT = 2;
+    private final static int REQUEST_ENABLE_LOC = 3;
+    private final static int REQUEST_ENABLE_BTADMIN = 4;
 
-    private final static int REQUEST_ENABLE_LOC = 777;
-
-    private final static int REQUEST_ENABLE_BTADMIN = 420;
+    private List<String> deviceList;
+    private ArrayAdapter<String> arrayAdapter;
 
     private BtleService.LocalBinder serviceBinder;
-
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-            else if (mBluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
-                mHelpText.setText("Discovery started :D");
-            }
-            else if (mBluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                mHelpText.setText("Discovery finsished..");
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,21 +63,47 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         setContentView(R.layout.activity_main);
 
         mHelpText =  (TextView) findViewById(R.id.HelpText);
+        mBluetoothList = (ListView) findViewById(R.id.LIbluetoothList);
+        mBlink = (Button) findViewById(R.id.button3);
+
+        //prepare the listview contents for button onclick
+        deviceList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, deviceList);
+        mBluetoothList.setAdapter(arrayAdapter);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        // Register for broadcasts when a device is discovered.
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
-
         mMetaWearButton = (Button) findViewById(R.id.button);
+        mBlink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Led led;
+                if ((led= board.getModule(Led.class)) != null) {
+                    led.editPattern(Led.Color.BLUE, Led.PatternPreset.BLINK)
+                            .repeatCount((byte) 10)
+                            .commit();
+                    led.play();
+                }
+            }
+        });
         mMetaWearButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //if were already discovering, cancel it first
                 if (mBluetoothAdapter.isDiscovering()) {
                     mBluetoothAdapter.cancelDiscovery();
                 }
                 mBluetoothAdapter.startDiscovery();
                 mHelpText.setText("Discovery should have started...");
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+//                if (pairedDevices.size() > 0) {
+//                    // There are paired devices. Get the name and address of each paired device.
+//                    for (BluetoothDevice device : pairedDevices) {
+//                        String deviceName = device.getName();
+//                        String deviceHardwareAddress = device.getAddress(); // MAC address
+//                        deviceList.add(deviceName + "        " + deviceHardwareAddress);
+//                    }
+//                    arrayAdapter.notifyDataSetChanged();
+//                }
             }
         });
         mPolarButton = (Button) findViewById(R.id.button2);
@@ -89,28 +112,25 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
 
             }
         });
+        mBluetoothList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String device = deviceList.get(position);
+                Toast.makeText(MainActivity.this, "Trying to connect with " + device, Toast.LENGTH_SHORT).show();
+                retrieveBoard(device);
+            }
+        });
 
-        //request permissions
-        int regularmanCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH);
-        int adminCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_ADMIN);
-        int locCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
-        getPermission();
-
-        //metawear
+        // Bind the service when the activity is created
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
-    }
 
-    public void getPermission(){
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                REQUEST_ENABLE_LOC);
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.BLUETOOTH},
-                REQUEST_ENABLE_BT);
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.BLUETOOTH_ADMIN},
-                REQUEST_ENABLE_BTADMIN);
+        //request permissions
+        requestPermissions();
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
     }
 
     @Override
@@ -120,19 +140,22 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             mHelpText.setText("El no supporto el tootho");
             return;
         }
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-        }
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+    }
+
+    public void requestPermissions(){
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_ENABLE_LOC);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.BLUETOOTH},
+                REQUEST_ENABLE_BT);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.BLUETOOTH_ADMIN},
+                REQUEST_ENABLE_BTADMIN);
     }
 
     @Override
@@ -153,6 +176,28 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         }
     }
 
+    public void retrieveBoard(String macAddress) {
+        final BluetoothManager btManager=
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        final BluetoothDevice remoteDevice=
+                btManager.getAdapter().getRemoteDevice(macAddress);
+
+        // Create a MetaWear board object for the Bluetooth Device
+        board = serviceBinder.getMetaWearBoard(remoteDevice);
+        //left off here, gl
+        board.connectAsync().continueWith(new Continuation<Void, Void>() {
+            @Override
+            public Void then(Task<Void> task) throws Exception {
+                if (task.isFaulted()) {
+                    Log.i("MainActivity", "Failed to connect");
+                } else {
+                    Log.i("MainActivity", "Connected");
+                }
+                return null;
+            }
+        });
+    }
+
     public void getPermissions() {
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(MainActivity.this,
@@ -170,6 +215,24 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
         }
     }
 
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                deviceList.add(deviceHardwareAddress);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+
+
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
         // Typecast the binder to the service's LocalBinder class
@@ -177,15 +240,14 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
     }
 
     @Override
-    public void onServiceDisconnected(ComponentName name) {
-
-    }
+    public void onServiceDisconnected(ComponentName componentName) { }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
-        // Don't forget to unregister the ACTION_FOUND receiver.
+        // unregister the ACTION_FOUND receiver.
         unregisterReceiver(mReceiver);
+        // Unbind the service when the activity is destroyed
         getApplicationContext().unbindService(this);
     }
 }
